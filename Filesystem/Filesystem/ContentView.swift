1dfcc6b40a99ef7b53eba9ea05ff073c76f35578
larry <xspanni@gmail.com>
2024-09-24 16:42:16 +0800

    增加访问文件系统，遍历文件夹并记录书签

---
+++ Filesystem/Filesystem/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Filesystem
+//
+//  Created by Larry on 2024/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FilesystemView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filesystem/Filesystem/FilesystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilesystemApp.swift
+//  Filesystem
+//
+//  Created by Larry on 2024/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FilesystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
